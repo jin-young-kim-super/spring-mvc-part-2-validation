62bc5481f6df42e44d20d0b2fded869989d8d874
Jin Young Kim <wlsdud66523@kakao.com>
2026-06-07 15:53:12 +0900

    検証の直接処理 - 開発

--- src/main/java/hello/itemservice/web/validation1/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation1/ValidationItemControllerV1.java
@@ -3,13 +3,18 @@ package hello.itemservice.web.validation1;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -38,7 +43,37 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+        // 검증 오류 데이터를 보관
+        Map<String,String> errors = new HashMap<>();
+
+        // 검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || (item.getPrice() < 1000 || item.getPrice() > 1000000)) {
+            errors.put("price","가격은 1000 ~ 1000000까지만 허용됩니다.");
+        }
+        if(item.getQuantity() == null || item.getQuantity() <= 9999) {
+            errors.put("quantity", "수량은 9999개까지만 허용됩니다.");
+        }
+
+        // 특정 필드 검증이 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000) {
+                errors.put("globalError", "가격*수량은 10000 이상이여야 합니다. 현재값 : " + resultPrice);
+            }
+        }
+
+        // 검증 실패하면 결과 데이터를 사용자에게 그대로 보내주워야 한다.
+        if(!errors.isEmpty()) {
+            log.info("errors={}",errors);
+            model.addAttribute("errors",errors);
+            return "/validation/v1/addForm";
+        }
+
+        // 여기서부터는 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -57,6 +92,4 @@ public class ValidationItemControllerV1 {
         itemRepository.update(itemId, item);
         return "redirect:/validation/v1/items/{itemId}";
     }
-
 }
-
